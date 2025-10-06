990998a51953876feb08efde66a4479016c3d556
Matt Micene (RHT) <mmicene@redhat.com>
2025-10-06 16:34:25 -0400

    Remove troubleshooting

--- setup-automation/setup-builder.sh
+++ setup-automation/setup-builder.sh
@@ -31,7 +31,7 @@ set +x
 certbot certonly --eab-kid "${ZEROSSL_EAB_KEY_ID}" --eab-hmac-key "${ZEROSSL_HMAC_KEY}" --server "https://acme.zerossl.com/v2/DV90" --standalone --preferred-challenges http -d builder-"${GUID}"."${DOMAIN}" --non-interactive --agree-tos -m trackbot@instruqt.com -v
 
 # Don't leak password to users
-#rm /var/log/letsencrypt/letsencrypt.log
+rm /var/log/letsencrypt/letsencrypt.log
 
 # reset tracing
 set -x
